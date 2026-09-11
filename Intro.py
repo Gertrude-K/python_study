b12e35f88501c301dc5741a5b795708f5f726cd8
Gertrude Karanja <gertrudekaranja10@gmail.com>
2026-09-11 19:19:58 +0300

    beginning

---
+++ Intro.py
@@ -0,0 +1,9 @@
+print("Gertrude")
+
+print (100)
+
+total = 120 + 390
+print(total)
+
+my_name = "gertrude"
+print(my_name)
---
+++ variables.py
@@ -0,0 +1,11 @@
+#calculate area of a square
+
+side = 50 
+area =side *side
+print(area)
+
+#calculate area of a rectangle
+len =56
+width =5
+area = len *width
+print(area)
